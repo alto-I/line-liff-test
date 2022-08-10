700794670fb91dc217d2a67ad868adaf1a901bab
alto-I <blmaldiy@gmail.com>
2022-08-10 14:46:31 +0900

    LINEログインしてユーザー情報を取得する

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,58 @@ function App() {
   const [login, setLogin] = useState('');
 
   useEffect(() => {
-    setVersion(liff.getLineVersion());
+    initLiff();
+  }, []);
+
+  const initLiff = () => {
     liff
-      .init({
-        liffId: import.meta.env.VITE_LIFF_ID,
-      })
+      .init({ liffId: import.meta.env.VITE_LIFF_ID })
       .then(() => {
-        setMessage('LIFF init succeeded.');
-        setLogin(liff.isLoggedIn().toString());
+        if (liff.isLoggedIn() === false) {
+          liff.login({});
+          getUserInfo();
+        }
       })
-      .catch((e) => {
-        setMessage('LIFF init failed.');
-        setError(`${e}`);
+      .catch((error) => {
+        console.error(error);
       });
-  });
+  };
+
+  const getUserInfo = () => {
+    liff
+      .getProfile()
+      .then((profile) => {
+        alert(JSON.stringify(profile));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  // useEffect(() => {
+  //   setVersion(liff.getLineVersion());
+  //   liff
+  //     .init({
+  //       liffId: import.meta.env.VITE_LIFF_ID,
+  //     })
+  //     .then(() => {
+  //       setMessage('LIFF init succeeded.');
+  //       setLogin(liff.isLoggedIn().toString());
+  //     })
+  //     .catch((e) => {
+  //       setMessage('LIFF init failed.');
+  //       setError(`${e}`);
+  //     });
+  // });
 
   return (
-    <div className="App">
-      <h1>create-liff-app</h1>
-      {message && <p>{message}</p>}
-      <p>line version: {version}</p>
-      {error && (
-        <p>
-          <code>{error}</code>
-        </p>
+    <>
+      {liff.isInClient() === false ? (
+        <p>ブラウザからは使えない。LIFF使え!!</p>
+      ) : (
+        <p>ようそこ LIFFアプリへ!</p>
       )}
-      <p>{login}</p>
-    </div>
+    </>
   );
 }
 
